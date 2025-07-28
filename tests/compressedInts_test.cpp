e801e0a56cfa39034dd6138c158a9ffd7e45488f
TimoWild <timo.wi@aol.com>
2025-07-28 19:49:59 +0200

    Improve test

--- tests/compressedInts_test.cpp
+++ tests/compressedInts_test.cpp
@@ -35,55 +35,78 @@ TEST(CompressedInts, ContainsValueName)
 
 TEST(CompressedInts, setAndGetValue)
 {
-    constexpr uint8_t V1_BITS_NEEDED_MAX = 16;
-    constexpr uint8_t V2_BITS_NEEDED_MAX = 16;
+    constexpr uint8_t V1_BITS_NEEDED_MAX = 9;
+    constexpr uint8_t V2_BITS_NEEDED_MAX = 9;
+    constexpr uint8_t V3_BITS_NEEDED_MAX = 9;
 
-    []<uint32_t... V1BitsNeededSeq>(std::integer_sequence<uint32_t, V1BitsNeededSeq...>)
+    []<uint32_t... V1BitsNeededSeq, uint32_t... V2BitsNeededSeq, uint32_t... V3BitsNeededSeq>(
+        std::integer_sequence<uint32_t, V1BitsNeededSeq...>, std::integer_sequence<uint32_t, V2BitsNeededSeq...>,
+        std::integer_sequence<uint32_t, V3BitsNeededSeq...>)
     {
-        const auto f = []<uint32_t V1BitsNeeded, uint32_t... V2BitsNeededSeq>(std::integer_sequence<uint32_t, V2BitsNeededSeq...>)
+        const auto f = []<uint32_t V1BitsNeeded>()
         {
             const auto f2 = []<uint32_t V2BitsNeeded>()
             {
-                compressedInts::CompressedInts<Values, {Values::V1, V1BitsNeeded}, {Values::V2, V2BitsNeeded}> test{};
-
-                test.template setValue<Values::V2>(0);
-
-                for (uint32_t i = 0; i < (1 << V1BitsNeeded); ++i)
+                const auto f3 = []<uint32_t V3BitsNeeded>()
                 {
-                    test.template setValue<Values::V1>(i);
-                    EXPECT_EQ(test.template getValue<Values::V1>(), i);
+                    compressedInts::CompressedInts<Values, {Values::V1, V1BitsNeeded}, {Values::V2, V2BitsNeeded}, {Values::V3, V3BitsNeeded}> test{};
 
-                    EXPECT_EQ(test.getData(), i);
-                }
+                    test.template setValue<Values::V2>(0);
+                    test.template setValue<Values::V3>(0);
 
-                test.template setValue<Values::V1>((1 << V1BitsNeeded) + 1);
-                EXPECT_EQ(test.template getValue<Values::V1>(), 1); // Overflow (upper bits will be cut off)
+                    for (uint32_t i = 0; i < (1 << V1BitsNeeded); ++i)
+                    {
+                        test.template setValue<Values::V1>(i);
+                        EXPECT_EQ(test.template getValue<Values::V1>(), i);
 
-                test.template setValue<Values::V1>(0);
+                        EXPECT_EQ(test.getData(), i);
+                    }
 
-                for (uint32_t i = 0; i < (1 << V2BitsNeeded); ++i)
-                {
-                    test.template setValue<Values::V2>(i);
-                    EXPECT_EQ(test.template getValue<Values::V2>(), i);
+                    test.template setValue<Values::V1>((1 << V1BitsNeeded) + 1);
+                    EXPECT_EQ(test.template getValue<Values::V1>(), 1); // Overflow (upper bits will be cut off)
 
-                    EXPECT_EQ(test.getData(), i << V1BitsNeeded);
-                }
+                    test.template setValue<Values::V1>(0);
 
-                test.template setValue<Values::V2>((1 << V2BitsNeeded) + 1);
-                EXPECT_EQ(test.template getValue<Values::V2>(), 1); // Overflow (upper bits will be cut off)
+                    for (uint32_t i = 0; i < (1 << V2BitsNeeded); ++i)
+                    {
+                        test.template setValue<Values::V2>(i);
+                        EXPECT_EQ(test.template getValue<Values::V2>(), i);
 
-                static_assert(std::is_same_v<typename TypeWithTotalBits<V1BitsNeeded + V2BitsNeeded>::type, decltype(test.getData())>);
+                        EXPECT_EQ(test.getData(), i << V1BitsNeeded);
+                    }
 
-                // Does not exist
-                test.template setValue<Values::V3>(9);
-                EXPECT_EQ(test.template getValue<Values::V3>(), 0);
+                    test.template setValue<Values::V2>((1 << V2BitsNeeded) + 1);
+                    EXPECT_EQ(test.template getValue<Values::V2>(), 1); // Overflow (upper bits will be cut off)
+
+                    test.template setValue<Values::V2>(0);
+
+                    for (uint32_t i = 0; i < (1 << V3BitsNeeded); ++i)
+                    {
+                        test.template setValue<Values::V3>(i);
+                        EXPECT_EQ(test.template getValue<Values::V3>(), i);
+
+                        EXPECT_EQ(test.getData(), i << (V1BitsNeeded + V2BitsNeeded));
+                    }
+
+                    test.template setValue<Values::V3>((1 << V3BitsNeeded) + 1);
+                    EXPECT_EQ(test.template getValue<Values::V3>(), 1); // Overflow (upper bits will be cut off)
+
+                    constexpr auto totalBits = V1BitsNeeded + V2BitsNeeded + V3BitsNeeded;
+
+                    if constexpr (totalBits <= 32)
+                    {
+                        static_assert(std::is_same_v<typename TypeWithTotalBits<totalBits>::type, decltype(test.getData())>);
+                    }
+                };
+
+                (f3.template operator()<V3BitsNeededSeq>(), ...);
             };
 
             (f2.template operator()<V2BitsNeededSeq>(), ...);
         };
 
-        ((f.template operator()<V1BitsNeededSeq>(
-             compressedInts::utils::integer_sequence_from_to<uint32_t, V1BitsNeededSeq, V2_BITS_NEEDED_MAX + 1>{})),
-         ...);
-    }(compressedInts::utils::integer_sequence_from_to<uint32_t, 1, V1_BITS_NEEDED_MAX + 1>{});
+        (f.template operator()<V1BitsNeededSeq>(), ...);
+    }(compressedInts::utils::integer_sequence_from_to<uint32_t, 1, V1_BITS_NEEDED_MAX + 1>{},
+      compressedInts::utils::integer_sequence_from_to<uint32_t, 1, V2_BITS_NEEDED_MAX + 1>{},
+      compressedInts::utils::integer_sequence_from_to<uint32_t, 1, V3_BITS_NEEDED_MAX + 1>{});
 }
